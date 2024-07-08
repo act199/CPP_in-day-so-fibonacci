11f0e7e3012ce2079271c33ebb71994c20b61683
SPC <SPC@17-LAB402>
2024-07-08 19:46:40 +0700

    Add project files.

---
+++ CPP_in day so fibonacci.cpp
@@ -0,0 +1,43 @@
+﻿// CPP_in day so fibonacci.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+// Hàm đệ quy tính số Fibonacci thứ n
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+// In ra các số Fibonacci từ đầu đến n
+    int Fibonacci(int n) {
+    for (int i = 0; i < n; ++i) {
+        cout << fibonacci(i) << " ";
+    }
+    cout << endl;
+}
+int main() {
+    int n;
+    cout << "Nhap phan tuc cua day fibonacci can in: ";
+    cin >> n;
+
+    cout << "Day fibonacci la: ";
+   fibonacci(n);
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
